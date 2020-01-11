16f36b567ac9340190127ab6e77908122582d9ba
Anderson <contatodeanderson@gmail.com>
2020-01-10 23:03:36 -0300

    add criarLista

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-alert('funfou');
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#container input');
+var btnElement = document.querySelector('#container button');
+
+var tarefas = ['Acordar', 'Escovar os dentes', 'Caminhar'];
+
+function criarLista() {
+    for (tarefa of tarefas) {
+        var tarefaElement       = document.createElement('li');
+        tarefaElement.className = 'collection-item';
+
+        var tarefaText          = document.createTextNode(tarefa);
+        var linkElement         = document.createElement('a');
+        linkElement.setAttribute('href', '#');
+        linkElement.className = 'secondary-content';
+
+        var linkText            = document.createTextNode('Excluir')
+        linkElement.appendChild(linkText);
+
+        tarefaElement.appendChild(tarefaText);
+        tarefaElement.appendChild(linkElement);
+        listElement.appendChild(tarefaElement);
+    }
+}
+
+criarLista();
